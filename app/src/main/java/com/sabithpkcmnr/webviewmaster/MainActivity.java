ff350d920a7922337a5516d7c471d27e49632d47
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2018-02-21 15:58:38 +0530

    Nothing to say, a project by Sabith Pkc Mnr.

---
+++ app/src/main/java/com/sabithpkcmnr/webviewmaster/MainActivity.java
@@ -0,0 +1,148 @@
+package com.sabithpkcmnr.webviewmaster;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView superImageView;
+    ProgressBar superProgressBar;
+    WebView superWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        superImageView = findViewById(R.id.myImageView);
+        superProgressBar = findViewById(R.id.myProgressBar);
+        superWebView = findViewById(R.id.myWebView);
+
+        superProgressBar.setMax(100);
+
+        superWebView.loadUrl("https://www.google.com");
+        superWebView.getSettings().setJavaScriptEnabled(true);
+        superWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+        superWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                superProgressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                superImageView.setImageBitmap(icon);
+
+            }
+        });
+
+        superWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Uri myUri = Uri.parse(url);
+                Intent superIntent = new Intent(Intent.ACTION_VIEW);
+                superIntent.setData(myUri);
+                startActivity(superIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater myMenuInflater = getMenuInflater();
+        myMenuInflater.inflate(R.menu.super_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.myMenuOne:
+                onBackPressed();
+                break;
+
+            case R.id.myMenuTwo:
+                GoForward();
+                break;
+
+            case R.id.myMenuThree:
+
+                break;
+
+        }
+        return true;
+    }
+
+    private void GoForward() {
+        if (superWebView.canGoForward()) {
+            superWebView.goForward();
+        } else {
+            Toast.makeText(this, "Can't go further!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (superWebView.canGoBack()) {
+            superWebView.goBack();
+        } else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Exit App");
+            dialog.setMessage("Browser has nothing to go back, so what next?");
+            dialog.setPositiveButton("EXIT ME", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            dialog.setCancelable(false);
+            dialog.setNegativeButton("STAY HERE", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+
+        }
+    }
+}
